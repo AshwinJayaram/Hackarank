582e47ad1f3ff5b550a15dc472d92aeda223fe01
AshwinJayaram <66546960+AshwinJayaram@users.noreply.github.com>
2023-03-27 15:04:32 +0530

    Kangaroo catching up
    This code is to check if the kangaroo  can catch up to the kangaroo in front of it

---
+++ Kangaroo.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+'''
+You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+
+The first kangaroo starts at location  and moves at a rate of  meters per jump.
+The second kangaroo starts at location  and moves at a rate of  meters per jump.
+You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+'''
+
+
+
+
+ def kangaroo(x1, v1, x2, v2):
+    k1=[]
+    k2=[]
+    if x2>x1 and v2>v1:
+        return "NO"
+    
+    for i in range(10000):
+       x1=x1+v1
+       k1.append(x1)
+       x2=x2+v2
+       k2.append(x2)
+    for j in k1:
+        if j in k2:
+            if k2.index(j)==k1.index(j):
+                return "YES"
+    else:
+        return "NO"
+        
+             
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
